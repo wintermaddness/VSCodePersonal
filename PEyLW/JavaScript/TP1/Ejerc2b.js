8f47a28459c35b91d0968445c0868191287622b1
Celeste Aliaga <celeste.aliaga@est.fi.uncoma.edu.ar>
2022-05-21 13:03:47 -0300

    Ejerc. TP JS PEyLW.

---
+++ PEyLW/JavaScript/TP1/Ejerc2b.js
@@ -0,0 +1,9 @@
+//Funciones que son llamadas desde el body.
+function enHead() {
+	alert("en Head")
+}
+function enExterno() {
+	alert("en Externo");
+}
+// captura de eventos, HTML din�mico, etc..
+alert("en Body");
