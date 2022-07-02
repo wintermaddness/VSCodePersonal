74cde1ae81612dde12562d6fd4240d94d52e665c
Celeste Aliaga <celeste.aliaga@est.fi.uncoma.edu.ar>
2022-07-01 23:19:17 -0300

    Cambios en los archivos TPFinal PEyLW (reseñas+fecha)

--- PEyLW/TPFinal/Trabajo Final PEyLW/registro.js
+++ PEyLW/TPFinal/Trabajo Final PEyLW/registro.js
@@ -4,9 +4,34 @@ let classes = (classes) => document.getElementsByClassName(classes);*/
 /* Elementos con ID */
 const nombre = document.getElementById("Nombre-de-Usuario")
 const email = document.getElementById("correo-electrónico")
+const date = document.getElementById("fecha-nac")
 const pass = document.getElementById("contrasena")
 const form = document.getElementById("formulario")
 const parrafo = document.getElementById("warnings")
+
+const calcularAnio = (e) => {
+    let esBisiesto = null;
+    if (e % 4 == 0) {
+        if (e % 100 == 0) {
+            if (e % 400 == 0) {
+                //El año ingresado es bisiesto:
+                esBisiesto = true;
+            } else {
+                //El año ingresado no es bisiesto:
+                esBisiesto = false;
+            }
+        } else {
+            //El año ingresado es bisiesto:
+            esBisiesto = true;
+        }
+    } else {
+       //El año ingresado no es bisiesto:
+        esBisiesto = false;
+    }
+    return esBisiesto;
+}
+
+const arrayMeses = [[31], [28], [31], [30], [31], [30], [31], [31], [30], [31], [30], [31]];
     
 form.addEventListener("submit", e => {
     e.preventDefault()
@@ -14,6 +39,7 @@ form.addEventListener("submit", e => {
     let entrar = false;
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
     let regexName = new RegExp('^[A-Z]+$','i');
+    let regexPattern = /^\d{1,2}\/\d{1,2}\/\d{2,4}$/;
     let regexPassword = /^[0-9]*$/;
     parrafo.innerHTML = "";
 
@@ -44,6 +70,48 @@ form.addEventListener("submit", e => {
         email.style.borderColor = 'palegreen';
     }
 
+    /* Validación de la fecha de nacimiento */
+    if (!regexPattern.test(date.value)) {
+        //La fecha debe ser d/m/a:
+        warnings += 'La fecha no es válida<br>(ingrese d/m/a)<br>';
+        date.style.borderColor = 'red';
+        //date.focus();
+        entrar = true;
+    } else {
+        date.style.borderColor = 'palegreen';
+    }
+
+    /*let arrayFecha = date.split('/');
+    let anio = parseInt(arrayFecha[2]);
+    let mes = parseInt(arrayFecha[1]) - 1;
+    let dia = parseInt(arrayFecha[0]);
+    //Se comprueba si es año bisiesto:
+    if (calcularAnio(anio)) {
+        //Si es año bisiesto:
+        arrayMeses[1] = 29;
+    } else {
+        arrayMeses[1] = 28;
+    }
+
+    //Se comprueba el mes:
+    if (mes <= 11 && mes >= 0) {
+        //Se comprueba la cantidad de días del mes:
+        let cantidadDias = parseInt(arrayMeses[mes]);
+        if (cantidadDias >= dia) {
+            date.style.borderColor = 'palegreen';
+        } else {
+            warnings += 'La fecha no es válida<br>(ingrese datos correctos)<br>';
+            date.style.borderColor = 'red';
+            date.focus();
+            entrar = true;
+        }
+    } else {
+        warnings += 'La fecha no es válida <br>(ingrese datos correctos)<br>';
+        date.style.borderColor = 'red';
+        date.focus();
+        entrar = true;
+    }*/
+
     /* Validación de la contraseña */
     if (!regexPassword.test(pass.value)) {
         //La contraseña debe ser sólo numérica:
@@ -68,6 +136,7 @@ form.addEventListener("submit", e => {
         parrafo.innerHTML = "Registro exitoso"
         nombre.style.borderColor = '';
         email.style.borderColor = '';
+        date.style.borderColor = '';
         pass.style.borderColor = '';
     }
 })
